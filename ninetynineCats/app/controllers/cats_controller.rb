f0cafaa20e486267bb6bd7fb0384201530307570
soura934 <soura934@gmail.com>
2020-12-04 17:13:35 -0500

    just render

--- ninetynineCats/app/controllers/cats_controller.rb
+++ ninetynineCats/app/controllers/cats_controller.rb
@@ -3,4 +3,9 @@ class CatsController < ApplicationController
         cats = Cat.all
         render json: cats
     end
+
+    def show
+        cat = Cat.find(params[:id])
+        render json: cat
+    end
 end
--- ninetynineCats/db/seeds.rb
+++ ninetynineCats/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create!(birth_date: '2015/01/20',color: 'green', name: 'kitty', sex: 'F', description: 'hello' )
+Cat.create!(birth_date: '2016/06/20',color: 'yellow', name: 'pickachu', sex: 'F', description: 'nice cat' )
+Cat.create!(birth_date: '2018/06/20',color: 'red', name: 'redcat', sex: 'M', description: 'bad cat' )
